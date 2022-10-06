7e9e5aa71e5e69e06b1a1060fa3e9cad1e8e7591
coky134 <rizkyhidayattullah94@gmail.com>
2022-10-06 10:47:56 +0700

    perapihan

--- scin.js
+++ scin.js
@@ -13,3 +13,56 @@ $(document).ready(function(){
         $(".isi3").slideToggle("slow");
     });
 });
+$(document).ready(function(){
+    $(".tombol4").click(function(){
+        $(".isi4").slideToggle("slow");
+    });
+});
+$(document).ready(function(){
+    $(".tombol5").click(function(){
+        $(".isi5").slideToggle("slow");
+    });
+});
+
+
+
+
+
+
+
+
+// $(document).ready(function(){
+//     $(".tombol").click(function(){
+//         $(".isi1").slideToggle("slow");
+//         $(".isi2").slideToggle("slow");
+//         $(".isi3").slideToggle("slow");
+//         $(".isi4").slideToggle("slow");
+//         $(".isi5").slideToggle("slow");
+//     });
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $(document).ready(function(){
+//     $(".tombol").click(function(){
+//         let id = $(this).prop('id');
+//         let split = id.split('_');
+//         let id_menu = split[1];
+
+//         $("isi_").slideToggle("slow");
+//     });
+// });
